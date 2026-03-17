ce3112b0348a20c9077cfdb83592f961f46271da
dinesh725 <dineshkhatua672@gmail.com>
2026-03-17 17:52:27 +0530

    fix: remove express-mongo-sanitize causing 500 type error crashes on all routes

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,6 @@ const config = require("./config");
 const { generalLimiter, sensitiveLimiter, financialLimiter } = require("./middleware/rateLimiter");
 const requestId = require("./middleware/requestId");
 const { abuseDetector } = require("./middleware/abuseDetector");
-const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
 const { AppError } = require("./utils/errors");
@@ -100,9 +99,6 @@ app.use(express.json({
     }
 }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
-
-// Data sanitization against NoSQL query injection
-app.use(mongoSanitize());
 
 // Data sanitization against XSS
 app.use(xss());
